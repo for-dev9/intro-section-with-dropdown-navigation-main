7b61c8da2832dda4e85b742d030370d778a6f34b
Aon <aon2018@outlook.com>
2023-01-10 16:22:06 +0700

    -

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+var hamburger = document.getElementById('mobileMenu');
+var closeMobileMenu = document.getElementById('closeMobileMenu');
+var mobileMenuDiv = document.getElementById('mobileMenuDiv');
+
 var subMenus = document.querySelectorAll('.subMenu');
 var menuIcons = document.querySelectorAll('.menuIcon');
 var menus = document.querySelectorAll('.menu');
 
+hamburger.addEventListener('click', hamburgerClick);
+closeMobileMenu.addEventListener('click', closeMobileMenuClick);
 menus.forEach((menu) => menu.addEventListener('click', onMenuClick));
 menus.forEach((menu) => menu.addEventListener('focusout', onMenuFocusout));
+
+function hamburgerClick() {
+  mobileMenuDiv.classList.toggle('-right-full');
+  mobileMenuDiv.classList.toggle('right-0');
+  hamburger.classList.toggle('hidden');
+  closeMobileMenu.classList.toggle('hidden');
+}
+
+function closeMobileMenuClick() {
+  mobileMenuDiv.classList.toggle('-right-full');
+  mobileMenuDiv.classList.toggle('right-0');
+  hamburger.classList.toggle('hidden');
+  closeMobileMenu.classList.toggle('hidden');
+}
 
 function onMenuClick(e) {
   const classString = e.target.getAttribute('data-target');
